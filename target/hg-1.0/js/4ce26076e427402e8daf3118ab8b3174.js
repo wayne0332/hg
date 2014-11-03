cb5e546cd1d12f0579649b265ae17ff19900629f
林江圣 <linjiangsheng@meituan.com>
2014-11-03 20:41:22 +0800

    remove target

--- target/hg-1.0/js/4ce26076e427402e8daf3118ab8b3174.js
+++
@@ -1,1 +0,0 @@
-(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=1b0ecf1d4b599a11921371fa56bc1e86&services=true&t=20141017112700"></script>');})();

710f3ffea22a87b21d05a1ba689b2a9418b81d1f
wayne0332 <527519996@qq.com>
2014-11-10 23:26:50 +0800

    add thanks

--- src/main/java/cn/hg/controller/MainController.java
+++ src/main/java/cn/hg/controller/MainController.java
@@ -10,6 +10,7 @@ import cn.hg.pojo.Message;
 import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,94 +25,89 @@ import static cn.hg.jooq.tables.Picture.PICTURE;
 import static cn.hg.jooq.tables.Recruit.RECRUIT;
 
 @RestController
-public class MainController
-{
-	private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+public class MainController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
-	@Resource
-	private DSLContext dsl;
+    @Resource
+    private DSLContext dsl;
 
-	@RequestMapping("")
-	public ModelAndView index() 
-	{
-		//公司简介文字
-		DescriptionRecord description = dsl.selectFrom(DESCRIPTION).where(DESCRIPTION.TYPE.equal(DescriptionType.INTRODUCE)).fetchOne();
-		//logo及logo图片
-		List<List<PictureRecord>> picture_bag = new ArrayList<List<PictureRecord>>();
-		for(int i =17;i<25;i++)
-		{
-			PictureType pt = PictureType.GENERATOR.getByIndex(i);
-			List<PictureRecord> pl = dsl.selectFrom(PICTURE).where(PICTURE.TYPE.eq(pt)).and(PICTURE.GROUP_ID.eq(Param.LOGO_GROUP_ID)).fetchInto(PictureRecord.class);
-			picture_bag.add(pl);
-		}
-		//实力展示轮播图片
-		List<PictureRecord> picture_list = dsl.selectFrom(PICTURE).where(PICTURE.GROUP_ID.eq(Param.STREGTH_GROUP_ID)).fetchInto(PictureRecord.class);
-		return new ModelAndView("fore/index").addObject("description", description).addObject(Param.PICTURE_BAG,picture_bag).addObject(Param.PICTURE_LIST,picture_list);
-	}
-	
-	@RequestMapping("aboutus")
-	public ModelAndView aboutus()
-	{
-		DescriptionRecord description = dsl.selectFrom(DESCRIPTION).where(DESCRIPTION.TYPE.equal(DescriptionType.INTRODUCE)).fetchOne();
-		return new ModelAndView("fore/aboutus").addObject("description", description);
-	}
-	
-	@RequestMapping("strength")
-	public ModelAndView strength(String type) 
-	{
-		//默认进入‘钣金车间’
-		if(null==type||"".equals(type))
-		{
-			type = "1";
-		}
-		PictureType pictureType = PictureType.GENERATOR.getByIndex(Integer.valueOf(type));
-		List<PictureRecord> picture_list =	dsl.selectFrom(PICTURE).where(PICTURE.TYPE.eq(pictureType)).and(PICTURE.GROUP_ID.eq(Param.STREGTH_GROUP_ID)).fetchInto(PictureRecord.class);
-		//前台最多显示4张图片
-		if(picture_list.size()>4)
-		{
-			picture_list = picture_list.subList(0, 3);
-		}
-		return new ModelAndView("fore/strength").addObject(Param.PICTURE_LIST, picture_list);
-	}
-	
-	@RequestMapping("case")
-	public ModelAndView cases(String type) {
-		//真实案例，默认进入‘立体发光字工程’
-		if(null==type||"".equals(type))
-		{
-			type="14";
-		}
-		PictureType pictureType = PictureType.GENERATOR.getByIndex(Integer.valueOf(type));
-		List<PictureRecord> picture_list = dsl.selectFrom(PICTURE).where(PICTURE.TYPE.eq(pictureType)).and(PICTURE.GROUP_ID.eq(Param.CASE_GROUP_ID)).fetchInto(PictureRecord.class);
-		return new ModelAndView("fore/case").addObject(Param.PICTURE_LIST, picture_list);
-	}
-	
-	@RequestMapping("contact")
-	public ModelAndView contact()
-	{
-		return new ModelAndView("fore/contact");
-	}
-	
-	@RequestMapping("joinus")
-	public ModelAndView joinus(String type) 
-	{
-		List<RecruitRecord> recruit_list = dsl.selectFrom(RECRUIT).orderBy(RECRUIT.ID).fetchInto(RecruitRecord.class);
-		if(null==type||"".equals(type))
-		{
-			type = recruit_list.size()>0?recruit_list.get(0).getId()+"":0+"";
-		}
-		RecruitRecord recruitRecord = dsl.selectFrom(RECRUIT).where(RECRUIT.ID.eq(Integer.valueOf(type))).fetchOne();
-		String description  = dsl.selectFrom(DESCRIPTION).where(DESCRIPTION.ID.eq(recruitRecord.getDescriptionId())).fetchOne().getText();
-		return new ModelAndView("fore/joinus").addObject(Param.RECRUIT_LIST,recruit_list).addObject(Param.RECRUIT_RECORD,recruitRecord).addObject(Param.RECRUIT_DESCRIPTION,description);
-	}
+    @RequestMapping("")
+    public ModelAndView index() {
+        //公司简介文字
+        DescriptionRecord description = dsl.selectFrom(DESCRIPTION).where(DESCRIPTION.TYPE.equal(DescriptionType.INTRODUCE)).fetchOne();
+        //logo及logo图片
+        List<List<PictureRecord>> picture_bag = new ArrayList<List<PictureRecord>>();
+        for (int i = 17; i < 25; i++) {
+            PictureType pt = PictureType.GENERATOR.getByIndex(i);
+            List<PictureRecord> pl = dsl.selectFrom(PICTURE).where(PICTURE.TYPE.eq(pt)).and(PICTURE.GROUP_ID.eq(Param.LOGO_GROUP_ID)).fetchInto(PictureRecord.class);
+            picture_bag.add(pl);
+        }
+        //实力展示轮播图片
+        List<PictureRecord> picture_list = dsl.selectFrom(PICTURE).where(PICTURE.GROUP_ID.eq(Param.STREGTH_GROUP_ID)).fetchInto(PictureRecord.class);
+        return new ModelAndView("fore/index").addObject("description", description).addObject(Param.PICTURE_BAG, picture_bag).addObject(Param.PICTURE_LIST, picture_list);
+    }
 
-	@RequestMapping(value = "/message",method = RequestMethod.GET)
-	public ModelAndView message() {
-		return new ModelAndView("fore/message");
-	}
+    @RequestMapping("aboutus")
+    public ModelAndView aboutus() {
+        DescriptionRecord description = dsl.selectFrom(DESCRIPTION).where(DESCRIPTION.TYPE.equal(DescriptionType.INTRODUCE)).fetchOne();
+        return new ModelAndView("fore/aboutus").addObject("description", description);
+    }
 
-	@RequestMapping(value = "/message",method = RequestMethod.POST)
-	public ModelAndView message(Message message) {
-		return new ModelAndView("fore/message");
-	}
+    @RequestMapping("strength")
+    public ModelAndView strength(String type) {
+        //默认进入‘钣金车间’
+        if (null == type || "".equals(type)) {
+            type = "1";
+        }
+        PictureType pictureType = PictureType.GENERATOR.getByIndex(Integer.valueOf(type));
+        List<PictureRecord> picture_list = dsl.selectFrom(PICTURE).where(PICTURE.TYPE.eq(pictureType)).and(PICTURE.GROUP_ID.eq(Param.STREGTH_GROUP_ID)).fetchInto(PictureRecord.class);
+        //前台最多显示4张图片
+        if (picture_list.size() > 4) {
+            picture_list = picture_list.subList(0, 3);
+        }
+        return new ModelAndView("fore/strength").addObject(Param.PICTURE_LIST, picture_list);
+    }
+
+    @RequestMapping("case")
+    public ModelAndView cases(String type) {
+        //真实案例，默认进入‘立体发光字工程’
+        if (null == type || "".equals(type)) {
+            type = "14";
+        }
+        PictureType pictureType = PictureType.GENERATOR.getByIndex(Integer.valueOf(type));
+        List<PictureRecord> picture_list = dsl.selectFrom(PICTURE).where(PICTURE.TYPE.eq(pictureType)).and(PICTURE.GROUP_ID.eq(Param.CASE_GROUP_ID)).fetchInto(PictureRecord.class);
+        return new ModelAndView("fore/case").addObject(Param.PICTURE_LIST, picture_list);
+    }
+
+    @RequestMapping("contact")
+    public ModelAndView contact() {
+        return new ModelAndView("fore/contact");
+    }
+
+    @RequestMapping("joinus")
+    public ModelAndView joinus(String type) {
+        List<RecruitRecord> recruit_list = dsl.selectFrom(RECRUIT).orderBy(RECRUIT.ID).fetchInto(RecruitRecord.class);
+        if (null == type || "".equals(type)) {
+            type = recruit_list.size() > 0 ? recruit_list.get(0).getId() + "" : 0 + "";
+        }
+        RecruitRecord recruitRecord = dsl.selectFrom(RECRUIT).where(RECRUIT.ID.eq(Integer.valueOf(type))).fetchOne();
+        String description = dsl.selectFrom(DESCRIPTION).where(DESCRIPTION.ID.eq(recruitRecord.getDescriptionId())).fetchOne().getText();
+        return new ModelAndView("fore/joinus").addObject(Param.RECRUIT_LIST, recruit_list).addObject(Param.RECRUIT_RECORD, recruitRecord).addObject(Param.RECRUIT_DESCRIPTION, description);
+    }
+
+    @RequestMapping(value = "/message", method = RequestMethod.GET)
+    public ModelAndView message() {
+        return new ModelAndView("fore/message");
+    }
+
+    @RequestMapping(value = "/message", method = RequestMethod.POST)
+    public ModelAndView message(Message message) {
+        return new ModelAndView("redirect:/thanks/" + message.getName());
+    }
+
+    @RequestMapping(value = "/thanks/{name}", method = RequestMethod.GET)
+    public ModelAndView thanks(@PathVariable String name)
+    {
+        return new ModelAndView("fore/thanks").addObject("name", name);
+    }
 }
